8dfc41671d2d2c38630823ff56229fb23c4a5cf7
xjunz <zxj19994@gmail.com>
2020-06-26 17:00:31 +0800

    ViewBinding for FilterFragment
    Signed-off-by: xjunz <zxj19994@gmail.com>

--- app/src/main/java/xjunz/tool/wechat/ui/activity/main/FilterFragment.java
+++ app/src/main/java/xjunz/tool/wechat/ui/activity/main/FilterFragment.java
@@ -2,7 +2,6 @@ package xjunz.tool.wechat.ui.activity.main;
 
 import android.os.Bundle;
 import android.text.Html;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +9,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,25 +19,18 @@ import androidx.lifecycle.ViewModelProvider;
 import java.util.Objects;
 
 import xjunz.tool.wechat.R;
+import xjunz.tool.wechat.databinding.FragmentFilterBinding;
 import xjunz.tool.wechat.ui.activity.main.model.FilterConfiguration;
 import xjunz.tool.wechat.ui.activity.main.model.FilterViewModel;
 import xjunz.tool.wechat.ui.activity.main.model.SortBy;
 import xjunz.tool.wechat.util.UiUtils;
 
 public class FilterFragment extends Fragment implements AdapterView.OnItemSelectedListener, View.OnClickListener, Observer<FilterConfiguration> {
-    private Spinner mSpName;
-    private Spinner mSpTimestamp;
-    private Spinner mSpMsgCount;
-    private Spinner mSpCategory;
-    private Spinner mSpSortBy;
-    private Spinner mSpOrderBy;
-    private TextView mTvStats;
     private FilterConfiguration mCurConfig;
     private Spinner[] mSpList;
     private FilterViewModel mModel;
-    private TextView mTvCaptionTime, mTvCaptionMsgCount;
     private Button mBtnConfirm, mBtnReset;
-
+    private FragmentFilterBinding mBinding;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -51,38 +42,26 @@ public class FilterFragment extends Fragment implements AdapterView.OnItemSelect
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_filter, container, true);
-        initViews(root);
+        mBinding = FragmentFilterBinding.inflate(inflater);
+        ViewGroup root = mBinding.getRoot();
+        initViews();
         return root;
     }
 
 
-    private void initViews(ViewGroup root) {
-        mSpOrderBy = root.findViewById(R.id.sp_order_by);
-        mSpSortBy = root.findViewById(R.id.sp_sort_by);
-        mSpCategory = root.findViewById(R.id.sp_category);
-        mSpMsgCount = root.findViewById(R.id.sp_msg_count);
-        mSpName = root.findViewById(R.id.sp_name);
-        mSpTimestamp = root.findViewById(R.id.sp_time);
-        mTvStats = root.findViewById(R.id.tv_stats);
-        mSpSortBy.setOnItemSelectedListener(this);
-        mSpOrderBy.setOnItemSelectedListener(this);
-        mSpList = new Spinner[]{mSpName, mSpTimestamp, mSpMsgCount};
+    private void initViews() {
+        mBinding.spSortBy.setOnItemSelectedListener(this);
+        mBinding.spOrderBy.setOnItemSelectedListener(this);
+        mBinding.spCategory.setOnItemSelectedListener(this);
+        mSpList = new Spinner[]{mBinding.spName, mBinding.spTime, mBinding.spMsgCount};
         for (int i = 0; i < mSpList.length; i++) {
             Spinner spinner = mSpList[i];
             spinner.setTag(SortBy.values()[i]);
+            spinner.setOnItemSelectedListener(this);
         }
-        mSpCategory.setOnItemSelectedListener(this);
-        mSpMsgCount.setOnItemSelectedListener(this);
-        mSpName.setOnItemSelectedListener(this);
-        mSpTimestamp.setOnItemSelectedListener(this);
-        mSpOrderBy.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.orders)));
-        mTvCaptionTime = root.findViewById(R.id.tv_caption_time);
-        mTvCaptionMsgCount = root.findViewById(R.id.tv_caption_msg_count);
-        mBtnConfirm = root.findViewById(R.id.btn_confirm_filter);
-        mBtnConfirm.setOnClickListener(this);
-        mBtnReset = root.findViewById(R.id.btn_reset_filter);
-        mBtnReset.setOnClickListener(this);
+        mBinding.spOrderBy.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.orders)));
+        mBinding.btnConfirmFilter.setOnClickListener(this);
+        mBinding.btnResetFilter.setOnClickListener(this);
     }
 
     @Override
@@ -106,27 +85,22 @@ public class FilterFragment extends Fragment implements AdapterView.OnItemSelect
                 mModel.config(mCurConfig, FilterConfiguration.PAYLOAD_INIT);
                 break;
             case FilterConfiguration.PAYLOAD_COUNT_CHANGED:
-                this.mTvStats.setEllipsize(TextUtils.TruncateAt.MARQUEE);
-                this.mTvStats.setSingleLine(true);
-                this.mTvStats.setMarqueeRepeatLimit(-1);
-                this.mTvStats.setFocusable(true);
-                this.mTvStats.setFocusableInTouchMode(true);
-                this.mTvStats.setText(Html.fromHtml(getString(R.string.format_count_stats, filterConfig.totalCount, filterConfig.filteredCount)));
+                mBinding.tvStats.setText(Html.fromHtml(getString(R.string.format_count_stats, filterConfig.totalCount, filterConfig.filteredCount)));
                 break;
             case FilterConfiguration.PAYLOAD_INIT:
                 if (mCurConfig.filterVictim == FilterConfiguration.VICTIM_CONTACT) {
-                    UiUtils.disable(mSpMsgCount, mSpTimestamp, mTvCaptionTime, mTvCaptionMsgCount);
-                    mSpName.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, Objects.requireNonNull(mCurConfig.descriptionListMap.get(SortBy.NAME))));
-                    mSpCategory.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.category_contact)));
-                    mSpSortBy.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.contact_sort)));
+                    UiUtils.disable(mBinding.spMsgCount, mBinding.spTime, mBinding.tvCaptionTime, mBinding.tvCaptionMsgCount);
+                    mBinding.spName.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, Objects.requireNonNull(mCurConfig.descriptionListMap.get(SortBy.NAME))));
+                    mBinding.spCategory.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.category_contact)));
+                    mBinding.spSortBy.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.contact_sort)));
                     Integer selection = mCurConfig.selectionMap.get(SortBy.NAME);
                     if (selection != null) {
-                        mSpName.setSelection(selection);
+                        mBinding.spName.setSelection(selection);
                     }
                 } else {
-                    UiUtils.enable(mSpMsgCount, mSpTimestamp, mTvCaptionTime, mTvCaptionMsgCount);
-                    mSpCategory.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.category_talker)));
-                    mSpSortBy.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.talker_sorts)));
+                    UiUtils.enable(mBinding.spMsgCount, mBinding.spTime, mBinding.tvCaptionTime, mBinding.tvCaptionMsgCount);
+                    mBinding.spCategory.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.category_talker)));
+                    mBinding.spSortBy.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.talker_sorts)));
                     for (Spinner spinner : mSpList) {
                         SortBy tag = (SortBy) spinner.getTag();
                         spinner.setAdapter(new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, Objects.requireNonNull(mCurConfig.descriptionListMap.get(tag))));
@@ -136,9 +110,9 @@ public class FilterFragment extends Fragment implements AdapterView.OnItemSelect
                         }
                     }
                 }
-                mSpCategory.setSelection(mCurConfig.categorySelection);
-                mSpSortBy.setSelection(mCurConfig.sortBySelection);
-                mSpOrderBy.setSelection(mCurConfig.ascending ? 0 : 1);
+                mBinding.spCategory.setSelection(mCurConfig.categorySelection);
+                mBinding.spCategory.setSelection(mCurConfig.sortBySelection);
+                mBinding.spOrderBy.setSelection(mCurConfig.ascending ? 0 : 1);
                 break;
         }
     }
